71925f8e00b5e717d98acc8353cc85852c66cbe6
wjx7794 <wjx7794@163.com>
2024-04-11 13:40:11 +0800

    feat: 注释v2

--- micro-react/src/index.js
+++ micro-react/src/index.js
@@ -8,6 +8,11 @@ import { BrowserRouter } from 'react-router-dom';
 import './public-path';
 
 let root = null;
+/**
+ * 「渲染函数」运行场景:
+ * 1. 主应用生命周期钩子中运行
+ * 2. 微应用单独启动时运行
+ */
 function render(props) {
   const { container } = props;
   console.log(document, container);
@@ -27,22 +32,39 @@ function render(props) {
   );
 }
 
+// 独立运行时
 if (!window.__POWERED_BY_QIANKUN__) {
   render({});
 }
 
+/**-------------------- 生命周期 start --------------------*/
+/**
+ * 1. bootstrap
+ * 时机: 只会在微应用初始化时调用一次，下次微应用重新进入时会直接调用 mount 钩子，不会再重复触发 bootstrap
+ * 应用: 在这里做一些全局变量的初始化，比如不会在 unmount 阶段被销毁的应用级别的缓存等
+ */
 export async function bootstrap() {
-  console.log('[react18] react app bootstraped');
+  console.log('🔥[react18] react app bootstraped>>>');
 }
 
+/**
+ * 2. mount
+ * 时机: 应用每次「进入」都会调用 mount 方法
+ * 应用: 在这里触发应用的渲染方法
+ */
 export async function mount(props) {
-  console.log('[react18] props from main framework', props);
+  console.log('🔥[react18] props from main framework>>>', props);
   render(props);
 }
 
+/**
+ * 3. unmount
+ * 时机: 应用每次「切出/卸载」会调用的方法
+ * 应用: 在这里会卸载微应用的应用实例
+ */
 export async function unmount(props) {
   root.unmount();
   root = null;
 }
-
+/**-------------------- 生命周期 end --------------------*/
 reportWebVitals();
--- micro-vue/src/main.js
+++ micro-vue/src/main.js
@@ -9,6 +9,11 @@ let instance = null;
 let history = null;
 
 const app = createApp(App);
+/**
+ * 「渲染函数」运行场景:
+ * 1. 主应用生命周期钩子中运行
+ * 2. 微应用单独启动时运行
+ */
 function render(props = {}) {
   const { container } = props;
 
@@ -28,29 +33,43 @@ if (!window.__POWERED_BY_QIANKUN__) {
   render({});
 }
 
+/**-------------------- 生命周期 start --------------------*/
 /**
- * bootstrap ： 在微应用初始化的时候调用一次，之后的生命周期里不再调用
+ * 1. bootstrap
+ * 时机: 只会在微应用初始化时调用一次，下次微应用重新进入时会直接调用 mount 钩子，不会再重复触发 bootstrap
+ * 应用: 在这里做一些全局变量的初始化，比如不会在 unmount 阶段被销毁的应用级别的缓存等
  */
 export async function bootstrap() {
-  console.log('vue3 bootstrap>>>');
+  console.log('🔥vue3 bootstrap>>>');
 }
 
 /**
- * 应用每次「进入」都会调用 mount 方法
- * - 通常我们在这里触发应用的渲染方法
+ * 2. mount
+ * 时机: 应用每次「进入」都会调用 mount 方法
+ * 应用: 在这里触发应用的渲染方法
  */
 export async function mount(props) {
-  console.log('vue3 mount>>>', props);
+  console.log('🔥vue3 mount>>>', props);
   render(props);
 }
 
 /**
- * 应用每次「切出/卸载」会调用的方法
- * - 通常在这里我们会卸载微应用的应用实例
+ * 3. unmount
+ * 时机: 应用每次「切出/卸载」会调用的方法
+ * 应用: 在这里会卸载微应用的应用实例
  */
-export async function unmount() {
-  console.log('vue3 unmount>>>');
+export async function unmount(props) {
+  console.log('🔥vue3 unmount>>>', props);
   app.unmount();
   instance = null;
   router = null;
 }
+
+/**
+ * 4. update
+ * 时机: 可选生命周期钩子，仅使用 loadMicroApp 方式加载微应用时生效
+ */
+export async function update(props) {
+  console.log('🔥vue3 update>>>', props);
+}
+/**-------------------- 生命周期 end --------------------*/
