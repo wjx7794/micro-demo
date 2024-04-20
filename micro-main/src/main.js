0c725dd74596c316aa95a8bd497313260c81e385
wjx7794 <wjx7794@163.com>
2024-04-20 21:04:56 +0800

    feat 抽离启动逻辑

--- micro-main/src/main.js
+++ micro-main/src/main.js
@@ -1,47 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import { registerMicroApps, start } from 'qiankun';
+// 注册微前端
+import './register';
 // qiankun 通信
 import './communication';
-
-// 1. 配置微应用
-const apps = [
-  {
-    name: 'reactApp',
-    entry: '//localhost:8082',
-    container: '#subapp-container',
-    activeRule: '/react',
-  },
-  {
-    name: 'vue3App',
-    entry: '//localhost:10000',
-    container: '#subapp-container',
-    activeRule: '/vue3',
-  },
-  {
-    name: 'vue2App',
-    entry: '//localhost:10200',
-    container: '#subapp-container',
-    activeRule: '/vue2',
-  },
-];
-
-// 2. 注册微应用
-registerMicroApps(apps, {
-  // qiankun 生命周期钩子 - 加载前
-  beforeLoad: (app) => {
-    console.log('beforeLoad>>>', app.name);
-    return Promise.resolve();
-  },
-  // qiankun 生命周期钩子 - 挂载后
-  afterMount: (app) => {
-    console.log('afterMount>>>', app.name);
-    return Promise.resolve();
-  },
-});
-
-// 3. 启动qiankun
-start();
 
 createApp(App).use(router).mount('#app');
---
+++ micro-main/src/register.js
@@ -0,0 +1,40 @@
+import { registerMicroApps, start } from 'qiankun';
+
+// 1. 配置微应用
+const apps = [
+  {
+    name: 'reactApp',
+    entry: '//localhost:8082',
+    container: '#subapp-container',
+    activeRule: '/react',
+  },
+  {
+    name: 'vue3App',
+    entry: '//localhost:10000',
+    container: '#subapp-container',
+    activeRule: '/vue3',
+  },
+  {
+    name: 'vue2App',
+    entry: '//localhost:10200',
+    container: '#subapp-container',
+    activeRule: '/vue2',
+  },
+];
+
+// 2. 注册微应用
+registerMicroApps(apps, {
+  // qiankun 生命周期钩子 - 加载前
+  beforeLoad: (app) => {
+    console.log('beforeLoad>>>', app.name);
+    return Promise.resolve();
+  },
+  // qiankun 生命周期钩子 - 挂载后
+  afterMount: (app) => {
+    console.log('afterMount>>>', app.name);
+    return Promise.resolve();
+  },
+});
+
+// 3. 启动qiankun
+start();
